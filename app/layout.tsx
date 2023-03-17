14532f86035b97990d398131c61b22c0f549a4c6
Sina-Abf <abf.sina@gmail.com>
2023-03-17 20:51:20 +0330

    comment metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,10 @@ import Header from "@/components/header";
 import "./globals.css";
 import ReactQueryWrapper from "./react-query-wrapper";
 
-export const metadata = {
-  title: "Morent Car Website",
-  description: "a Car salling website",
-};
+// export const metadata = {
+//   title: "Morent Car Website",
+//   description: "a Car salling website",
+// };
 
 export default function RootLayout({
   children,
